46b09047a5aa7a67226367be447904ad43aeeefb
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-05 15:36:40 +0100

    printed icons into page

---
+++ main.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+//     milestone 1:
+// definire un array di oggetti; ogni oggetto
+// rappresenta un'icona, che è caratterizzata da:
+// nome, prefisso, tipo e famiglia.
+
+
+//definisco array di oggetti
+
+    const icons = [
+        {
+            name: 'cat',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'crow',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dog',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dove',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dragon',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'horse',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'hippo',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'fish',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'carrot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'apple-alt',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'lemon',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'pepper-hot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'user-astronaut',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-graduate',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-ninja',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-secret',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        }
+    ];
+
+    // Utilizzando la funzione forEach e il template
+    // literal, visualizzare in pagina tutte le icone con il
+    // proprio nome.
+
+    icons.forEach((thisIcon) => {
+        console.log(thisIcon);
+        const {name, prefix, type, family} = thisIcon;
+        // console.log(thisIcon);
+        $('.icon-container').append(`
+            <div class='icons'>
+                <i class="${family} ${prefix}${name} fa-2x"></i>
+                <p>${name}</P>
+            </div>`);
+    });
+
+
+
+});
